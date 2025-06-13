4cc819fa853cffa45d6fbc799fcb145271e09b56
CanadianGamer <39717944+EasyCanadianGamer@users.noreply.github.com>
2025-06-13 14:39:36 +0000

    Update projexnexa.rb

--- Formula/projexnexa.rb
+++ Formula/projexnexa.rb
@@ -1,8 +1,8 @@
 class Projexnexa < Formula
   desc "Tauri application for project management"
   homepage "https://github.com/EasyCanadianGamer/ProjexNexa"
-  url "https://github.com/EasyCanadianGamer/ProjexNexa/releases/download/V0.1.5/ProjexNexa.app.tar.gz"
-  sha256 "fbc7dca19c031e59e47467171324d0800ddd1fea1ad0d605914b06d1e2fd6788"
+  url "https://github.com/EasyCanadianGamer/ProjexNexa/releases/download/v0.1.5/ProjexNexa.app.tar.gz"
+  sha256 "sha256:fbc7dca19c031e59e47467171324d0800ddd1fea1ad0d605914b06d1e2fd6788"
   license "MIT"
 
   depends_on macos: :monterey
